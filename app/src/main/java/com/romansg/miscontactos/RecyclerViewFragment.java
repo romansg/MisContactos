1e9e72ba805e74200965a3775e00536e41b3d217
romansg <romans@unam.mx>
2018-03-20 10:29:54 -0600

    Agregados dos fragments en blanco.

---
+++ app/src/main/java/com/romansg/miscontactos/RecyclerViewFragment.java
@@ -0,0 +1,22 @@
+package com.romansg.miscontactos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by roman on 20/03/18.
+ */
+
+public class RecyclerViewFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_recyclerview, container, false);
+        return v;
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
